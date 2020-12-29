2eba8d3ba7f99ebb12afee92cd1fe762255ccc6a
Tyler Farhner <tylerfarhner@gmail.com>
2020-12-29 13:13:06 -0800

    cleans up file

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,32 +1,5 @@
 // const movie = require('../models/movie');
 const Movie = require('../models/movie')
-// import { getOne } from '../models/movie'
-//  ^^ Forgot this guy
-
-
-
-// function create(req, res) {
-//   // remove whitespace next to commas
-//   req.body.cast = req.body.cast.replace(/\s*,\s*/g, ',');
-//   // split if it's not an empty string
-//   if (req.body.cast) req.body.cast = req.body.cast.split(',');
-
-//   const movie = new Movie(req.body);
-
-//   movie.save(function (err) {
-//     // one way to handle errors
-//     if (err) return res.render('movies/new');
-//     console.log(movie);
-//     // for now, redirect right back to new.ejs
-//     res.redirect('/movies');
-//   });
-// }
-
-// function index(req, res) {
-//   Movie.find({}, function(err, movies) {
-//     res.render('movies/index', { movies })
-//   })
-// }
 
 function newMovie(req, res) {
   res.render('movies/new')
@@ -58,18 +31,6 @@ function show(req, res) {
   });
 }
 
-// function show(req, res) {
-//   Movie.getOne({}, function(err, movies) {
-//     res.render('movies/show', )
-//   })
-// }
-
-// function show(req, res) {
-//   console.log(req.params.id)
-//   res.render('movies/show', {
-//     movie: Movie.getOne(req.params.id)
-//   })
-// }
 
 function deleteMovie(req, res) {
   Movie.findByIdAndDelete(req.params.id, function (err, movie) {
@@ -84,7 +45,5 @@ module.exports = {
   show,
   new: newMovie,
   create,
-  // Movie,
   delete: deleteMovie,
-  // ^^ this too :)
 }
